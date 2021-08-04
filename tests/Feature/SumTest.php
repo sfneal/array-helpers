788d588d35180cf1277a247373d8f46ff89fa20f
Stephen Neal <stephen.neal14@gmail.com>
2021-08-04 15:27:12 -0400

    REFACTOR `randomArray()` closure into a private method

--- tests/Feature/SumTest.php
+++ tests/Feature/SumTest.php
@@ -7,35 +7,43 @@ use Sfneal\Helpers\Arrays\Tests\TestCase;
 
 class SumTest extends TestCase
 {
+    /**
+     * Generate a random test array.
+     *
+     * @param int $numberOfArrays
+     * @param int $arrayDepth
+     * @return array[]
+     */
+    private function randomArray(int $numberOfArrays = 2, int $arrayDepth = 4): array
+    {
+        $array = [];
+        $expected = [];
+
+        foreach (range(0, $numberOfArrays) as $index) {
+            $set = [];
+            foreach (range(0, $arrayDepth) as $i) {
+                $set[$i] = rand(0, 1000);
+
+                if (! array_key_exists($i, $expected)) {
+                    $expected[$i] = 0;
+                }
+                $expected[$i] += $set[$i];
+            }
+            $array[$index] = $set;
+        }
+
+        return [$array, $expected];
+    }
+
     // todo: add ability to pass more arrays
     public function sumArrayProvider(): array
     {
-        $randArrays = function (int $numberOfArrays = 2, int $arrayDepth = 4) {
-            $array = [];
-            $expected = [];
-
-            foreach (range(0, $numberOfArrays) as $index) {
-                $set = [];
-                foreach (range(0, $arrayDepth) as $i) {
-                    $set[$i] = rand(0, 1000);
-
-                    if (! array_key_exists($i, $expected)) {
-                        $expected[$i] = 0;
-                    }
-                    $expected[$i] += $set[$i];
-                }
-                $array[$index] = $set;
-            }
-
-            return [$array, $expected];
-        };
-
         return [
-            $randArrays(),
-            $randArrays(),
-            $randArrays(),
-            $randArrays(),
-            $randArrays(),
+            $this->randomArray(),
+            $this->randomArray(),
+            $this->randomArray(),
+            $this->randomArray(),
+            $this->randomArray(),
         ];
     }
 
