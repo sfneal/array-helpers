d5671d3a69e2d3e36f6fe4ad4fefc00434f8c462
Stephen Neal <stephen.neal14@gmail.com>
2021-08-04 13:36:58 -0400

    REFACTOR `DiffFlatTest` custom assertion methods to `TestCase`

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,4 +4,31 @@ namespace Sfneal\Helpers\Arrays\Tests;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
+    // todo: refactor to sfneal/mock-models
+
+    /**
+     * Assert that an array has a value.
+     *
+     * @param $needle
+     * @param array $haystack
+     */
+    public function assertArrayHasValue($needle, array $haystack)
+    {
+        $this->assertTrue(
+            in_array($needle, array_values($haystack)),
+            "Could not find '{$needle}' in the array ".json_encode($haystack));
+    }
+
+    /**
+     * Assert that an array does not have a value.
+     *
+     * @param $needle
+     * @param array $haystack
+     */
+    public function assertArrayNotHasValue($needle, array $haystack)
+    {
+        $this->assertFalse(
+            in_array($needle, array_values($haystack)),
+            "Could not find '{$needle}' in the array ".json_encode($haystack));
+    }
 }
